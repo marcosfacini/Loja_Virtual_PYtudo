e4ab8e4a1e6947c2bbf01a06da488588f52af991
marcos <marcosfacini5@gmail.com>
2023-10-29 02:04:44 -0300

    profile style

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('excluir_usuario/<int:id>', views.excluir_usuario, name='excluir_usuario'),
     path('ver_usuario/<int:id>', views.ver_usuario, name='ver_usuario'),
     path('atualizar_usuario/<int:id>', views.atualizar_usuario, name='atualizar_usuario'),
+    path('perfil_usuario/<int:user_id>', views.perfil_usuario, name='perfil_usuario'),
+    path('usuario_atualiza_cadastro/<int:id>', views.usuario_atualiza_cadastro, name='usuario_atualiza_cadastro'),
 ]
