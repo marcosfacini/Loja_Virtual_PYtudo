0dec773a5191a03e61d4ddca4d1e34126a6392d8
marcos <marcosfacini5@gmail.com>
2024-02-27 20:50:22 -0300

    configuracoes nginx

--- scripts/comandos.sh
+++ scripts/comandos.sh
@@ -13,4 +13,4 @@ echo "Postgres iniciado com sucesso ($POSTGRES_HOST $POSTGRES_PORT)"
 python manage.py collectstatic --noinput
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
-python manage.py runserver 0.0.0.0:8000
+gunicorn pytudo_docker.wsgi:application --bind 0.0.0.0:8000
