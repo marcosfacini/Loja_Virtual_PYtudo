30d8ae16a7f5a8468af379b2169b3bf366c76a7b
marcos <marcosfacini5@gmail.com>
2024-03-02 09:41:20 -0300

    criar superuser uma vez

--- scripts/comandos.sh
+++ scripts/comandos.sh
@@ -13,4 +13,5 @@ echo "Postgres iniciado com sucesso ($POSTGRES_HOST $POSTGRES_PORT)"
 python manage.py collectstatic --noinput
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
+DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python manage.py createsuperuser --username $SUPER_USER_NAME --email $SUPER_USER_EMAIL --noinput
 gunicorn pytudo.wsgi:application --bind 0.0.0.0:8000
