f5f428fad26b74089f93268c76a217ef8c1b5964
marcos <marcosfacini5@gmail.com>
2024-03-13 17:01:16 -0300

    notificationtype branch-dev

--- checkout/serializers.py
+++ checkout/serializers.py
@@ -2,4 +2,4 @@ from rest_framework import serializers
 
 class NotificationSerializer(serializers.Serializer):
     notificationCode = serializers.CharField()
-    
+    notificationType = serializers.CharField()
